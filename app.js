f71f5726dc02d50696a200b516c43abdac8f3334
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-21 21:15:48 +0600

    module-37 begins

---
+++ app.js
@@ -0,0 +1,27 @@
+// let number = 9;
+// number = 5;
+// console.log(number);
+
+// const num = 5;
+// num = 3;
+// console.log(num);
+const friends = ["abul", "babul", "kabul", "kamal", "hero"];
+// friends.splice(1, 4);
+// friends.slice(2,3);
+
+// console.log(friends);
+// for (let i = 0; i < friends.length; i++) {
+//   const result = friends[i];
+//   const output = result.slice(3)
+//   console.log(output);
+
+// }
+
+const shobName = (a, b) => {
+  const sum = a + b;
+  return sum;
+ 
+};
+const output = shobName(3456, 235);
+
+console.log(output);
